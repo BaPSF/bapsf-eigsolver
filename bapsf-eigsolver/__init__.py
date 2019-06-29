f92c9a63e3a27e8d72b0b230d0e2cd149be736d5
rocco8773 <eteverson@gmail.com>
2019-06-28 17:58:24 -0700

    fill out package __init__; add __version__='1.0.0'

---
+++ bapsf-eigsolver/__init__.py
@@ -0,0 +1,13 @@
+# This file is part of the bapsf-egisolver package.
+#
+# http://plasma.physics.ucla.edu/
+#
+# Copyright 2019 Erik T. Everson and contributors
+#
+# --- Public API -------------------------------------------------------
+__all__ = ['eigsolver']
+
+from . import eigsolver
+
+# --- Define version ---------------------------------------------------
+__version__ = '1.0.0'
