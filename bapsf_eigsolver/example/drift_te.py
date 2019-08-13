5dafe849a9796bba4c124d1ddf48be5a3d20a776
a-shetty <51727819+a-shetty@users.noreply.github.com>
2019-08-13 00:53:36 -0700

    Add files via upload

---
+++ bapsf_eigsolver/example/drift_te.py
@@ -0,0 +1,15 @@
+
+
+import eigsolver_te
+
+metric = 'cyl'   # choose cylindrical ('cyl') or slab ('cart') geometry
+equation = eigsolver_te.SymbolicEq(metric) # Derive the eigenvalue equation in symbolic form
+
+p = eigsolver_te.PhysParams(Nr=100, np=3, tp='LAPD_nonrotating', mtheta=20.)  # define a set of physical
+                      # parameters for the problem (size, profiles, etc)
+
+esolver = eigsolver_te.EigSolve(equation, p)  # Solve the eigenvalue problem
+eigsolver_te.plot_omega(esolver)  # Plot profiles and eigenmodes
+
+# Result:
+# Fastest growing mode: omega= (-0.0656901535535+0.0318964836453j)
